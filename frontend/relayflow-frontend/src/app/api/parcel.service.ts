d4491351ab9dfcbb472e0cc0a99dc9c70dd9e125
Sabrine Hammami <HammamiSabrine933@gmail.com>
2026-02-03 20:18:12 +0100

    UI : Boutons de changement de statut

--- frontend/relayflow-frontend/src/app/api/parcel.service.ts
+++ frontend/relayflow-frontend/src/app/api/parcel.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { createParcelRequet, Parcel } from "./parcel.model";
+import { createParcelRequet, Parcel, ParcelStatus } from "./parcel.model";
 
 @Injectable({providedIn: "root"})
 export class ParcelService {
@@ -12,7 +12,10 @@ constructor(private http :HttpClient) {
 create (req:createParcelRequet) : Observable<Parcel> {
     return this.http.post<Parcel>(this.baseUrl , req);
 }
-getByReference(reference :String) :Observable<Parcel> {
-    return this.http.get<Parcel>(this.baseUrl+`/by-reference/${reference}`)
+getByReference(reference :string) :Observable<Parcel> {
+    return this.http.get<Parcel>(`${this.baseUrl}/by-reference/${encodeURIComponent(reference)}`);
+}
+updateStatus(reference : string , status : ParcelStatus) {
+return this.http.patch<Parcel> (`${this.baseUrl}/${encodeURIComponent(reference)}/status` , {status})
 }
 }
